a629df56b09f5b731558fb4ff3c111f39f1600c3
zql <zhangql@civaonline.cn>
2016-09-26 17:00:50 +0800

    Initial commit

---
+++ app/src/gradleTestA/java/com.testgradle/ToashUtils.java
@@ -0,0 +1,19 @@
+package com.testgradle;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * [description about this class]
+ *
+ * @author zhangqil
+ * @DATE 2016-09-26 15:26
+ * @copyright Copyright 2010 RD information technology Co.,ltd.. All Rights Reserved.
+ */
+
+public class ToashUtils {
+
+    public static void showToash(Context context) {
+        Toast.makeText(context, "AAA", Toast.LENGTH_SHORT).show();
+    }
+}
